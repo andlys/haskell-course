06fe4cc2f09f0bf82e4292c06631f903b1787b69
Andrii Lysenko <senkoily@gmail.com>
2017-09-19 11:44:16 +0300

    add exercises, fix existing

--- lysenko02.hs
+++ lysenko02.hs
@@ -1,15 +1,12 @@
 {-# OPTIONS_GHC -Wall #-}
 module Lysenko02 where
 
--- Код - просто список символів - десяткових цифр '0' ..'9'
 type Code = String
 
--- Крок гри (Move) будує конструктор Move використовуючи спробу (Code) і два цілих:  
---    кількість "биків" і "корів"  у пропозиції-спробі по відношенню до коду-числа 
 data Move = Move Code Int Int
           deriving (Show, Eq)
 
--- Задача 1 -----------------------------------------
+-- 1 -----------------------------------------
 exactMatches :: Code -> Code -> Int
 exactMatches _ [] = 0
 exactMatches [] _ = 0
@@ -20,7 +17,7 @@ test:
 exactMatches "1589" "5891"
 exactMatches "1589" "1159"
 -}
--- Задача 2 -----------------------------------------
+-- 2 -----------------------------------------
 countDigits :: Code -> [Int]
 countDigits cd = (map (\n -> 
                           (countif (\chr ->
@@ -38,37 +35,61 @@ test:
 countDigits "1589"
 countDigits "1117"
 -}
--- Задача 3 ----------------------------------------- 
+-- 3 ----------------------------------------- 
 matches :: Code -> Code -> Int
-matches cd att = foldl max 0
+matches cd att = foldl (+) 0
                      (merge (countDigits cd)
                             (countDigits att))
 
 merge :: [Int] -> [Int] -> [Int]
 merge [] _ = []
 merge _ [] = []
-merge (x:xs) (y:ys) = [x + y] ++ (merge xs ys)
+merge (x:xs) (y:ys) = (min x y) : (merge xs ys)
 {-
 test:
 matches "1589" "1158"
 -}
--- Задача 4 -----------------------------------------
+-- 4 -----------------------------------------
+-- TODO: refactor
 getMove :: Code -> Code -> Move
-getMove = undefined
-
--- Задача 5 -----------------------------------------
+getMove cd att = let
+                   f = (exactMatches cd att)
+                   p = (matches cd att)
+                 in (Move att f (p - f))
+{- test
+getMove "1589" "1158" 
+-}
+-- 5 -----------------------------------------
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
-
--- Задача 6 -----------------------------------------
+isConsistent (Move att f p) cd = (Move att f p) == (getMove cd att)
+{- test
+isConsistent (Move "2257" 1 1) "2519"  = True
+isConsistent (Move "2257" 1 1) "2529"  = False
+-}
+-- 6 -----------------------------------------
 filterCodes :: Move -> [Code] -> [Code]
 filterCodes = undefined
 
--- Задача 7 -----------------------------------------
+-- 7 -----------------------------------------
 allCodes :: Int -> [Code]
-allCodes = undefined
-   
--- Задача 7 -----------------------------------------
+allCodes n = comb n []
+
+-- adds all possible digits to a list of strings
+comb :: Int -> [String] -> [String]
+comb 0 lst = lst
+comb n []  = comb (n - 1)
+                  (addDigits [])
+comb n lst = comb (n - 1)
+                  (concat
+                      (map (\str -> addDigits str)
+                           lst))
+
+-- adds all possible digits to a string and returns a list of these
+addDigits :: String -> [String]
+addDigits str = map (\chr -> str ++ [chr])
+                    ['0','1','2','3','4','5','6','7','8','9']
+
+-- 8 -----------------------------------------
 solve :: Code -> [Move]
 solve = undefined
  
