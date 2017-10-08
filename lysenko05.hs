d9713190a78fd421d0e7aa72f287d3c57483f4b7
Andrii Lysenko <senkoily@gmail.com>
2017-10-08 14:39:26 +0300

    all exercises done

--- lysenko05.hs
+++ lysenko05.hs
@@ -143,11 +143,12 @@ instance Num AbstractInteger  where
 
 -- Задача 6 -----------------------------------------
 factorial :: (Eq a, Num a) => a -> a
-factorial n | n == 1    = 1
+factorial n | n == 0    = 1
             | otherwise = n * factorial (n - 1)
 
 {-
 --test
+factorial Zero
 factorial (Succ Zero)
 factorial (Succ (Succ Zero))
 factorial (Succ (Succ (Succ Zero))) == 6
@@ -160,22 +161,43 @@ factorial (Succ 6) == 5040
 -- Задача  7 -----------------------------------------
 data Quaternion = Quaternion Double Double Double Double deriving (Eq)
 
+-- getter
+getReal :: Quaternion -> Double
+getReal (Quaternion a _ _ _) = a
+
 instance Show Quaternion where
-    show = undefined
+    show (Quaternion a i j k) =
+      show a ++
+      (if i >= 0 then "+" else "") ++ show i ++ "i" ++
+      (if j >= 0 then "+" else "") ++ show j ++ "j" ++
+      (if k >= 0 then "+" else "") ++ show k ++ "k"
+-- show (Quaternion 4 5 (-6.89) 7) == "4.0+5.0i-6.89j+7.0k"
 
 -- Задача 8 -----------------------------------------
 plusQuaternion :: Quaternion -> Quaternion -> Quaternion
-plusQuaternion = undefined
+plusQuaternion (Quaternion a1 b1 c1 d1) (Quaternion a2 b2 c2 d2) =
+  (Quaternion (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2))
 
 -- Задача 9 -----------------------------------------
 timesQuaternion :: Quaternion -> Quaternion -> Quaternion
-timesQuaternion = undefined
+timesQuaternion (Quaternion a1 b1 c1 d1) (Quaternion a2 b2 c2 d2) =
+  (Quaternion (a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2)
+              (a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2)
+              (a1 * c2 - b1 * d2 + c1 * a2 + d1 * b2)
+              (a1 * d2 + b1 * c2 - c1 * b2 + d1 * a2))
+
+{-tests
+show (plusQuaternion (Quaternion 1 1 1 1) (Quaternion 2 2 2 6)) == "3.0+3.0i+3.0j+7.0k"
+show (timesQuaternion (Quaternion 1 1 1 1) (Quaternion 2 2 2 6)) == "-8.0+8.0i+0.0j+8.0k"
+-}
 
 --- Задача 10 ----------------------------------------
 instance Num Quaternion  where
     (+)   = plusQuaternion
     (*)   = timesQuaternion
-    negate      = undefined
-    fromInteger = undefined
-    abs         = undefined
-    signum      = undefined
+    a - b   = a + (fromInteger (-1)) * b
+    fromInteger n = Quaternion (fromIntegral n) 0 0 0
+    negate      q = (fromInteger 0) - q
+    abs (Quaternion a b c d) = Quaternion (sqrt $ a*a + b*b + c*c + d*d) 0 0 0
+    signum q@(Quaternion a b c d) = let k = getReal $ abs q
+                                    in Quaternion (a/k) (b/k) (c/k) (d/k)
